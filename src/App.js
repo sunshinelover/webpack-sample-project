c7cf6962bad5beb4760755c83ab863fc3a6c6992
yulin chen <yulinchen@L-R1ZKLVDL-1304.local>
2020-03-31 15:03:19 +0800

    feat:init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <p>React here!123</p>
+        </div>
+    );
+};
+export default App;
+ReactDOM.render(<App/>, document.getElementById("app"));
